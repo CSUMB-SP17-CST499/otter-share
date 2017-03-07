5b4785278a1b2417aa033ac20883146601bc9c38
Mason <maslopez@csumb.edu>
2017-03-07 08:59:38 -0500

    tweaked callback names

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
+/**
+    CST 499 - Final Capstone Project
+    server.js
+    Purpose: Communication between Android App and Neo4j DB, using Express, Node and neo4j drivers.
+
+    @author Mason Lopez
+    @version 1.1 4/10/16
+*/
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var neo4j = require('neo4j-driver').v1;
 
-//---> Credentials for connecting to GRAPHENEDB with Heroku!
+  //---> Credentials for connecting to GRAPHENEDB with Heroku!
    var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
    var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
    var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
    var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
-//
+  //
 
   const port = process.env.PORT || 3000;
   //----> Local credentials
   //var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "root"));
   var session = driver.session();
   var app = express();
-  app.use(bodyParser.json()); //uses bodyParser middleware
+  app.use(bodyParser.json()); //uses bodyParser middleware for reading body
 
-  // Tests a fake email hard-coded in source. Can delete this block now
   app.get('/', (req,res) => {
+    //may serve up OtterShare website later.
     res.status(200).send("Welcome to OtterShare, nothing to GET, though");
   });
-  app.get('/getTest', (req,res,next) => {
+
+  // Tests a fake email hard-coded in source.
+  app.get('/emailGet', (req,res,next) => {
     session
       .run("MATCH (a:Person) WHERE a.email <> {email} RETURN a.name AS name, a.email AS email, a.location AS location",
           {email: "Test e-mail90"})
@@ -42,7 +53,7 @@ var neo4j = require('neo4j-driver').v1;
       });
 });
 
-// Essentially DROPS database !
+// Essentially DROPS data from database ! For testing purposes only!!!
 app.get('/reset',(req,res,next) => {
   session
   .run("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
@@ -53,8 +64,8 @@ app.get('/reset',(req,res,next) => {
 });
 
 // adds certain strings POSTING to db.
-app.post('/postTest',(req,res,next) => {
-  // manipulate body fields, if not null we insert data!
+app.post('/insert',(req,res,next) => {
+  // manipulate body fields, if not null we create/insert data!
   var name = null;
   var email = null;
   var location = null;
@@ -62,7 +73,7 @@ app.post('/postTest',(req,res,next) => {
   name = req.body.name;
   email = req.body.email;
   location = req.body.location;
-  // console.log(req.body.name);
+
   if(!!name && !!email && !!location){
     session
       .run("CREATE (a:Person {name: {name}, email: {email}, location: {location} })", {name: name, email: email, location: location})
@@ -77,7 +88,7 @@ app.post('/postTest',(req,res,next) => {
     res.send('Fail, yo!');
   }
 });
-//searches for user by email, returns email.
+// Searches for user by email, returns email.
 app.post('/findByEmail', (req,res,next) => {
   var email = null;
   email = req.body.email;
