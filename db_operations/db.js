bcfa8f7101cdca87133817c7c3992d6667ca93f2
Mason <maslopez@csumb.edu>
2017-03-15 15:01:48 -0400

    Switched over to heroku credentials

--- db_operations/db.js
+++ db_operations/db.js
@@ -4,13 +4,13 @@
 */
 var neo4j = require('neo4j-driver').v1;
 //----> Local credentials
-var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "root"));
+//var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "root"));
 //
 // ---> Credentials for connecting to GRAPHENEDB with Heroku!
-// var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
-// var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
-// var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
-// var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
+var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
+var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
+var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
+var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
 //
 var session = driver.session();
 var bcrypt = require('bcrypt');
