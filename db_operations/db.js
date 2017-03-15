8220cc61cdb8c32d6802b0ef17a1854044608248
Mason <maslopez@csumb.edu>
2017-03-15 14:47:46 -0400

    Sorry for the lack of increment, but I cleaned up the server.js file by moving all db operations to the db_operations folder. Also added password hashing.

---
+++ db_operations/db.js
@@ -0,0 +1,78 @@
+/*
+  Handles all db operations
+
+*/
+var neo4j = require('neo4j-driver').v1;
+//----> Local credentials
+var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "root"));
+//
+// ---> Credentials for connecting to GRAPHENEDB with Heroku!
+// var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
+// var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
+// var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
+// var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
+//
+var session = driver.session();
+var bcrypt = require('bcrypt');
+
+//Takes email and password, searches neo4j db for them, if found, returns name, location and hashed pw -> FOR TESTING ONLY! need to remove!
+const findByEmailPw = (email, password, callback) => {
+    session
+        .run("MATCH (a:User) WHERE a.email = {email} RETURN a.name AS name, a.email AS email, a.location AS location, a.password AS password", {
+            email: email
+        })
+        .then((result) => {
+            let result_string = '';
+            let hash = null;
+            result.records.forEach((record) => {
+                // Prints the password field console.log(record._fields[3]);
+                hash = record._fields[3];
+                result_string += record._fields + ' ';
+            });
+            console.log('then ' + result_string);
+            bcrypt.compare(password, hash, function(err, res) {
+                if (res == true) {
+                    return callback(null, result_string);
+                }
+                return callback(null, null);
+            });
+        })
+        .catch((e) => {
+            console.log(e);
+            return callback('error : ' + JSON.stringify(e));
+        });
+}
+// need to develop auth key here, before insertion (creation) of user.
+const createUser = (email, name, location, password, callback) => {
+    // next step -> run a match for looking at email/authkey, if it finds it, then we return failure, if not, then we create user.
+    session
+        .run("CREATE (a:User {name: {name}, email: {email}, password: {password} ,location: {location} })", {
+            name: name,
+            email: email,
+            password: password,
+            location: location
+        })
+        .then(() => {
+            let succ = ("Success! ---> " + name + ' ' + email + ' ' + location + ' ' + password);
+            return callback(null, succ);
+        })
+        .catch((e) => {
+            return callback('error : ' + JSON.stringify(e));
+        });
+}
+
+const resetDB = (callback) => {
+    session
+        .run("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
+        .then(() => {
+            return callback(null, 'success');
+        })
+        .catch((e) => {
+            return callback(null, e);
+        });
+}
+module.exports = {
+    findByEmailPw,
+    createUser,
+    resetDB
+};
--- server.js
+++ server.js
@@ -4,121 +4,108 @@
     Purpose: Communication between Android App and Neo4j DB, using Express, Node and neo4j drivers.
 
     @author Mason Lopez
-    @version 1.1 4/10/16
-*/
+    @version 1.1 3/15/2017
+**/
 var express = require('express');
 var bodyParser = require('body-parser');
-var neo4j = require('neo4j-driver').v1;
+var db = require('./db_operations/db');
+var bcrypt = require('bcrypt');
 
-//---> Credentials for connecting to GRAPHENEDB with Heroku!
- var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
- var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
- var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
- var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
-//
+var app = express();
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
 
 const port = process.env.PORT || 3000;
-//----> Local credentials
-// var driver = neo4j.driver("bolt://localhost:7687", neo4j.auth.basic("neo4j", "root"));
-//
-var session = driver.session();
-var app = express();
-app.use(bodyParser.json()); //uses bodyParser middleware for reading body
 
+app.use(bodyParser.json()); //uses bodyParser middleware for reading body
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(expressValidator());
+
+var settings = {
+    secret: "asdfqwerty784334",
+    cookie: {},
+    saveUninitialized: false,
+    resave: false
+};
+app.use(expressSession(settings));
+
+//route points
 app.get('/', (req, res) => {
     //may serve up OtterShare website later.
     res.status(200).send("Welcome to OtterShare, nothing to GET, though");
 });
-
-// Tests a fake email hard-coded in source.
-app.get('/emailGet', (req, res, next) => {
-    session
-        .run("MATCH (a:Person) WHERE a.email <> {email} RETURN a.name AS name, a.email AS email, a.location AS location", {
-            email: "Test e-mail90"
-        })
-        .then(function(result) {
-            //let jsonMsg = result.records[0].get("title") + " " + result.records[0].get("name");
-            //var obj = JSON.parse('{ "name":"' + jsonMsg + '"}');
-            if (!result) {
-                return res.send("No match!");
-            }
-            let store = '';
-            for (x in result.records) {
-                store += JSON.stringify(result.records[x]);
-            }
-            res.send(store);
-            console.log(result.records);
-        })
-        .catch((e) => {
-            console.log('--- error below ---' + (JSON.stringify(e)));
-        });
+//demonstrates maintained session.
+app.get('/connected', (req, res) => {
+    //res.send('In connect');
+    if (req.session.user)
+        res.send('in connected ' + req.session.user);
+    else
+        res.status(401).send('Unauthorized!');
 });
 
-// Essentially DROPS data from database ! For testing purposes only!!!
-app.get('/reset', (req, res, next) => {
-    session
-        .run("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
-        .then(res.send('Succceeeessss!'))
-        .catch((e) => {
-            res.send('fail: ' + JSON.stringify(e));
+app.post('/login/authenticate', (req, res) => {
+    // Search for name in db
+    // need to check for CSUMB email
+    // will need to check for email & auth key instead, next goal
+    if (!!req.body.email && !!req.body.password) {
+        //run findByEmailPw callback function, if found w/ matching pw, authenticate
+
+        // Send in hashed pw, for comparison
+        db.findByEmailPw(req.body.email, req.body.password, (err, user) => {
+            if (err) {
+                res.send(err);
+            }
+            // sets session to user.
+            if (!!user) {
+                req.session.user = user;
+                res.redirect('/connected');
+            } else {
+                res.send('User not found');
+            }
         });
+
+    } else {
+        console.log(req.body.email);
+    }
 });
 
-// adds certain strings POSTING to db.
-app.post('/insert', (req, res, next) => {
-    // access body fields, if not null we create/insert data!
-    var name = null;
-    var email = null;
-    var location = null;
+app.post('/createUser', (req, res) => {
+    // Search for name in db
+    // need to check for CSUMB email
+    // app just needs to start here by sending email, right now I have email. No pw yet
+    var name = null || req.body.name;
+    var email = null || req.body.email;
+    var location = null || req.body.location;
+    var password = null || req.body.password;
 
-    name = req.body.name;
-    email = req.body.email;
-    location = req.body.location;
-
-    if (!!name && !!email && !!location) {
-        session
-            .run("CREATE (a:Person {name: {name}, email: {email}, location: {location} })", {
-                name: name,
-                email: email,
-                location: location
-            })
-            .then(() => {
-                res.send("Success! ---> " + name + ' ' + email + ' ' + location);
-            })
-            .catch((e) => {
-                console.log('error: ' + JSON.stringify(e));
+    // need to check for session as well, via auth key, will after testing.
+    if (!!name && !!email && !!location && !!password) {
+        // hashes password via response from callback, stored in hash!
+        bcrypt.hash(password, 12, (err, hash) => {
+            // Store hash in password DB, as well as all other fields.
+            let hashedPassword = hash;
+            db.createUser(email, name, location, hashedPassword, (err, succ) => {
+                if (err) {
+                    res.send(err);
+                }
+                res.send(succ);
             });
-    } else {
-        res.send('Fail, yo!');
+        });
     }
 });
-// Searches for user by email, returns email, name and location .
-app.post('/findByEmail', (req, res, next) => {
-    var email = null;
-    email = req.body.email;
-
-    if (!!email) {
-        session
-            .run("MATCH (a:Person) WHERE a.email = {email} RETURN a.name AS name, a.email AS email, a.location AS location", {
-                email: email
-            })
-            .then((result) => {
-                let result_string = '';
-
-                result.records.forEach((record) => {
-                    console.log(record._fields);
-                    result_string += record._fields + ' ';
-                    //delimits by comma
-                });
-                res.send(result_string);
-            })
-            .catch((e) => {
-                console.log('error : ' + JSON.stringify(e));
-            })
-    } else {
-        res.status(400).send("Email must be in correct format!!!");
-    }
-});
+// Essentially DROPS data from database ! For testing purposes only!!!
+// app.get('/reset', (req, res, next) => {
+//     db.resetDB((err, succ) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.send(succ);
+//         }
+//     });
+// });
+//begins listening on port 3000 or instance given port .
 app.listen(port, () => {
     console.log("Started on port " + port);
 });
