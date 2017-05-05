c4df4fe3814cd95c63495a858807a2cf1759a8d7
Babak Chehraz <bchehraz@csumb.edu>
2017-05-04 21:55:25 -0700

    initial commit for main async task

---
+++ app/src/main/java/com/ottershare/ottershare/MainTask.java
@@ -0,0 +1,11 @@
+package com.ottershare.ottershare;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by User on 5/4/2017.
+ */
+
+public class MainTask extends AsyncTask<String, String, String> {
+    
+}
