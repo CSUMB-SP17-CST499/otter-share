8b1a27b93c5974ce2e23e20b967ca3c9615f5d39
Babak Chehraz <bchehraz@csumb.edu>
2017-05-17 21:14:08 -0700

    edited waitforselltask to be more of a template for waitforbuyer

--- app/src/main/java/com/ottershare/ottershare/WaitForSellTask.java
+++ app/src/main/java/com/ottershare/ottershare/WaitForSellTask.java
@@ -6,7 +6,21 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by ryan on 5/14/17.
@@ -17,7 +31,7 @@ public class WaitForSellTask extends AsyncTask<String,String,Integer>{
     /* Context being passed in from main thread*/
     Context context;
     Activity prevActivity;
-    private String LOG_TAG =  WaitForSell.class.getSimpleName();
+    private String LOG_TAG =  WaitForSellTask.class.getSimpleName();
 
 
     public WaitForSellTask(Activity activity){
@@ -36,8 +50,52 @@ public class WaitForSellTask extends AsyncTask<String,String,Integer>{
         String passInput = params[1];
 
         try {
+            final String LOGIN_BASE_URL = "https://young-plains-98404.herokuapp.com/";
+            final String LOGIN_PARAM = "login";
+            final String EMAIL_PARAM = "email";
+            final String PASSWORD_PARAM = "password";
 
+            URL url = new URL(LOGIN_BASE_URL + LOGIN_PARAM);
 
+            //set up http connection
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setReadTimeout(10000);
+            httpURLConnection.setConnectTimeout(15000);
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setDoInput(true);
+            httpURLConnection.setDoOutput(true);
+
+            HashMap<String, String> postDataParams = new HashMap<>();
+
+            postDataParams.put(EMAIL_PARAM, emailInput);
+            postDataParams.put(PASSWORD_PARAM, passInput);
+
+            OutputStream os = httpURLConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int responseCode = httpURLConnection.getResponseCode();
+
+            Log.d(LOG_TAG, "Response code: " + responseCode);
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+
+                while ((line = br.readLine()) != null) {
+                    response += line;
+                    Log.d(LOG_TAG, response);
+                }
+            } else {
+                return -1;
+            }
+
+            JSONObject jsonResponse = new JSONObject(response);
+            //parse json
 
         }catch (Exception e){
             Log.e(LOG_TAG, e.getMessage());
@@ -56,5 +114,22 @@ public class WaitForSellTask extends AsyncTask<String,String,Integer>{
     @Override
     protected void onPostExecute(Integer result){
 
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
     }
 }
