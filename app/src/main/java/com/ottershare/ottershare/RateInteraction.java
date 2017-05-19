c49544927bbbfd1e8eda2754062b22d5cca403b4
RGrab <rgrabar@csumb.edu>
2017-05-18 18:54:58 -0700

    setup the rate interaction java and xml files to be connected to the backend at a later time

--- app/src/main/java/com/ottershare/ottershare/RateInteraction.java
+++ app/src/main/java/com/ottershare/ottershare/RateInteraction.java
@@ -1,19 +1,46 @@
 package com.ottershare.ottershare;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 
 public class RateInteraction extends AppCompatActivity {
 
     Button submitBtn;
+    SharedPreferences prefs;
+    String passId;
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_interaction);
 
-        //submitBtn = (Button) findViewById(R.id.)
+        submitBtn = (Button) findViewById(R.id.submit_btn);
+
+        submitBtn.setOnClickListener(listener);
+        ratingBar = (RatingBar) findViewById(R.id.rating_bar);
     }
+
+    View.OnClickListener listener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            Intent i;
+            switch (v.getId())
+            {
+                case R.id.submit_btn:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    };
 }
